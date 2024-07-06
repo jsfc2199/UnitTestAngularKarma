a9a65342da0a3dff7167c8bffe65bd0036b512c6
Juanse Franco <jsfc2199@gmail.com>
2024-07-05 22:43:41 -0500

    prueba sobre el html del componente

--- src/app/intermedio-integracion/incrementador/incrementador.component.spec.ts
+++ src/app/intermedio-integracion/incrementador/incrementador.component.spec.ts
@@ -1,27 +1,30 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { IncrementadorComponent } from './incrementador.component';
 import { FormsModule } from '@angular/forms';
-
+import { By } from '@angular/platform-browser';
 
 describe('Incremendator Component', () => {
+  let component: IncrementadorComponent;
+  let fixture: ComponentFixture<IncrementadorComponent>;
 
-    let component: IncrementadorComponent;
-    let fixture: ComponentFixture<IncrementadorComponent>;
-
-    beforeEach( () => {
-        TestBed.configureTestingModule({
-            declarations: [ IncrementadorComponent ],
-            imports: [ FormsModule ]
-        });
-
-        fixture = TestBed.createComponent(IncrementadorComponent);
-        component = fixture.componentInstance;
-
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [IncrementadorComponent],
+      imports: [FormsModule],
     });
 
-    it('', () => {
+    fixture = TestBed.createComponent(IncrementadorComponent);
+    component = fixture.componentInstance;
+  });
 
+  it('Debe de mostrar la leyenda', () => {
+    component.leyenda = 'progreso de carga';
+    const elem: HTMLElement = fixture.debugElement.query(
+      By.css('h3')
+    ).nativeElement;
 
-    });
-
+    //esperamos a que se detecten los cambios primero
+    fixture.detectChanges();
+    expect(elem.innerHTML).toContain('progreso');
+  });
 });
