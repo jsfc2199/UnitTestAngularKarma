f51e1a7d528a2da9257fc85233e934d9d30fe2cb
Juanse Franco <jsfc2199@gmail.com>
2024-07-05 21:25:13 -0500

    uso de spyOn para hacer una llamada falsa al servicio

--- src/app/intermedio/espias/medicos.component.spec.ts
+++ src/app/intermedio/espias/medicos.component.spec.ts
@@ -1,20 +1,28 @@
 import { MedicosComponent } from './medicos.component';
 import { MedicosService } from './medicos.service';
-
+import { from } from 'rxjs';
 
 describe('MedicosComponent', () => {
+  let componente: MedicosComponent;
 
-    let componente: MedicosComponent;
+  //instancia del servicio, y enviamos null porque no nos interesa usar nada del http
+  const servicio = new MedicosService(null);
 
-    beforeEach( () => {
-        
+  beforeEach(() => {
+    componente = new MedicosComponent(servicio);
+  });
+
+  it('init debe de cargar los médicos', () => {
+    const medicos = ['medico1', 'medico2', 'medico3'];
+
+    //usamos espías para hacer peticiones falsas
+    spyOn(servicio, 'getMedicos').and.callFake(() => {
+      return from([medicos]);
     });
 
+    //el ngOnInit lo debemos llamar de manera manual a menos de que estuviera en e constructor del componente
+    componente.ngOnInit();
 
-    it('', () => {
-
-   
-    });
-
-
+    expect(componente.medicos.length).toBeGreaterThan(0);
+  });
 });
