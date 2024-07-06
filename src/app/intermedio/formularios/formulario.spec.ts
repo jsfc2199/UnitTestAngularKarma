81e51cc65797c5ae52da3b64cf7c8be5d0748367
Juanse Franco <jsfc2199@gmail.com>
2024-07-05 20:56:36 -0500

    pruebas sobre validaciones en formularios

--- src/app/intermedio/formularios/formulario.spec.ts
+++ src/app/intermedio/formularios/formulario.spec.ts
@@ -1,17 +1,26 @@
-import { FormBuilder } from "@angular/forms";
-import { FormularioRegister } from "./formulario";
+import { FormBuilder } from '@angular/forms';
+import { FormularioRegister } from './formulario';
 
 describe('pruebas sobre formularios', () => {
-  let component: FormularioRegister
+  let component: FormularioRegister;
   beforeEach(() => {
-    component = new FormularioRegister(new FormBuilder())
+    component = new FormularioRegister(new FormBuilder());
   });
 
   it('debe de CREAR UN FORMULARIO CON 2 CAMPOS', () => {
+    expect(component.form.contains('email')).toBeTruthy();
+    expect(component.form.contains('password')).toBeTruthy();
+  });
 
-    expect(component.form.contains('email')).toBeTruthy()
-    expect(component.form.contains('password')).toBeTruthy()
+  it('el email debe de ser obligatorio', () => {
+    const control = component.form.get('email');
+    control?.setValue('');
+    expect(control?.valid).toBeFalsy();
+  });
 
-  })
-
+  it('el email debe de ser un correo valido', () => {
+    const control = component.form.get('email');
+    control?.setValue('valido@gmail.com');
+    expect(control?.valid).toBeTruthy();
+  });
 });
